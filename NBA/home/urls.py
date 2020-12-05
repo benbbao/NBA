623d500b6376e87e71c7a8f5fc7f4ab46d5ffed8
Ben <benbbao@aol.com>
2020-12-04 23:18:27 -0500

    final

--- NBA/home/urls.py
+++ NBA/home/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='homepage'),
+    path('playersHome/', views.playersHome, name='players-homepage'),
+    path('teamsHome/', views.teamsHome, name='teams-homepage'),
+    path('comparePlayers/', views.comparePlayers, name='compare-players'),
+    path('compareTeams/', views.compareTeams, name='compare-teams'),
+    path('watchlist/', views.watchlist, name='watchlist'),
 ]
--- NBA/home/views.py
+++ NBA/home/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/home.html')
+
+def playersHome(request):
+    return render(request, 'home/playersHome.html')
+
+def teamsHome(request):
+    return render(request, 'home/teamsHome.html')
+
+def comparePlayers(request):
+    return render(request, 'home/comparePlayers.html')
+
+def compareTeams(request):
+    return render(request, 'home/compareTeams.html')
+
+def watchlist(request):
+    return render(request, 'home/watchlist.html')
