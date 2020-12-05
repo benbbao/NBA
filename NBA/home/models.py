ffc87c7be58bc33211616806ba1eb2d9194d737c
Ben <benbbao@aol.com>
2020-12-04 23:18:47 -0500

    final

--- NBA/home/models.py
+++ NBA/home/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class PlayerStats(models.Model):
+    name = models.CharField(primary_key=True, max_length=100)
+    team = models.CharField(max_length=100)
+    position = models.CharField(max_length=5)
+    gp = models.IntegerField()
+    mpg = models.FloatField()
+    ftper = models.FloatField()
+    ppg = models.FloatField()
+    rpg = models.FloatField()
+    apg = models.FloatField()
+    spg = models.FloatField()
+    bpg = models.FloatField()
+    topg = models.FloatField()
+
+    class Meta:
+        managed = False
+        db_table = 'player_stats'
+
+class TeamStats(models.Model):
+    city = models.CharField(primary_key=True, max_length=50)
+    conference = models.CharField(max_length=5)
+    gp = models.IntegerField()
+    ppg = models.FloatField()
+    papg = models.FloatField()
+    sos = models.FloatField()
+    wins = models.IntegerField()
+    loses = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'team_stats'
+
+
--- NBA/home/views.py
+++ NBA/home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from home.models import PlayerStats
+from home.models import TeamStats
+
 def home(request):
     return render(request, 'home/home.html')
 
@@ -15,5 +18,29 @@ def comparePlayers(request):
 def compareTeams(request):
     return render(request, 'home/compareTeams.html')
 
-def watchlist(request):
-    return render(request, 'home/watchlist.html')
+def watchlist3(request):
+    return render(request, 'home/watchlist3.html')
+
+def watchlist1(request):
+    return render(request, 'home/watchlist1.html')
+
+def singleplayer(request):
+    return render(request, 'home/singleplayer.html')
+
+def singleteam(request):
+    return render(request, 'home/singleteam.html')
+
+def teamsearch(request):
+    return render(request, 'home/teamsSearch.html')
+
+def utahjazz(request):
+    return render(request, 'home/utahJazz.html')  
+
+def watchlist2(request):
+    return render(request, 'home/watchlist2.html')  
+
+def comparePlayers2(request):
+    return render(request, 'home/comparePlayers2.html')
+
+def comparePlayers3(request):
+    return render(request, 'home/comparePlayers3.html')
